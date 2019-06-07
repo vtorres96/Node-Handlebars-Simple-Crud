0e94ad88fb9056e0cc88e315504bc4be13fc424e
vtorres96 <victorcdc96@gmail.com>
2019-06-07 19:20:44 -0300

    criando Model Posts

---
+++ models/posts.js
@@ -0,0 +1,16 @@
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize('node_mysql', 'victor', '03v01t96m', {
+    host: 'localhost',
+    dialect: 'mysql'
+})
+
+const Post = sequelize.define('posts', {
+    title: {
+        type: Sequelize.STRING
+    },
+    content: {
+        type: Sequelize.TEXT
+    }
+})
+
+
