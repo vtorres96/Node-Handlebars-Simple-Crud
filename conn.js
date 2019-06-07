d654275d1dcdc533e6287c719e2dd98caba3fbef
vtorres96 <victorcdc96@gmail.com>
2019-06-07 01:16:35 -0300

    estabelendo conexao utilizando Sequelize

---
+++ conn.js
@@ -0,0 +1,11 @@
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize('node_mysql', 'victor', '03v01t96m', {
+    host: 'localhost',
+    dialect: 'mysql'
+})
+
+sequelize.authenticate().then(
+    () => console.log("Conecado com sucesso")
+).catch(
+    (erro) => console.log("Falha ao conectar " + erro)
+)
