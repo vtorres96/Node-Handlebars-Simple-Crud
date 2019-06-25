f799f794e7dfc3a9ce18ebf985c750ea0baeab7e
vtorres96 <victorcdc96@gmail.com>
2019-06-25 14:43:03 -0300

    creating a new file with all routes

---
+++ routes.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const PostController = require('./controllers/PostController')
+
+const routes = new express.Router()
+
+// Home
+routes.get("/", PostController.home)
+
+// Posts - Create
+routes.get("/cadastro", PostController.cadastroPost)
+routes.post("/salvar-cadastro", PostController.salvarCadastroPost)
+
+// Posts - Update
+routes.get("/alterar-post/:id", PostController.alteracaoDePost)
+routes.put('/alterar-post/:id', PostController.salvarAlteracaoDePost)
+
+// Post - Delete
+routes.get("/deletar/:id", PostController.exclusaoDePost)
+
+module.exports = routes
